6df4eb2f81237abf4d98a9a34313f653f4443100
morgane <mcabrol21@gmail.com>
2017-06-02 14:54:58 +0200

    creation d'une liste fictive

--- projectIF25Final/src/Main.java
+++ projectIF25Final/src/Main.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ArrayList<MonUser> list = calculFreqTweet();
+        System.out.println("Hello World!" + list.toString());
+
+    }
+
+    private static ArrayList<MonUser> calculFreqTweet(){
+        ArrayList<MonUser> list = new ArrayList<MonUser>();
+        for(int i=0;i<30;i++){
+            if(i % 2 == 0){   //Je conidère que un chiffre est atypique si il est paire
+                MonUser user = new MonUser(i,true);
+                list.add(user);
+            }else{
+                MonUser user = new MonUser(i,false);
+                list.add(user);
+            }
+
+
+        }
+        return list;
+    }
+
+    private static class MonUser{
+        int chiffre;
+        boolean estAtypique;
+        public MonUser(int ch, boolean atip){
+            this.chiffre = ch;
+            this.estAtypique = atip;
+        }
+
+        @Override
+        public String toString() {
+            return "MonUser{" +
+                    "chiffre=" + chiffre +
+                    ", estAtypique=" + estAtypique +
+                    '}';
+        }
     }
 }
+
+
