9b21c7c1bca50dc440c477084d67d4475544c90a
morgane <mcabrol21@gmail.com>
2017-06-17 15:41:39 +0200

    jar JSON ajouté

--- projectIF25Final/src/ParseurJSON.java
+++ projectIF25Final/src/ParseurJSON.java
@@ -1,5 +1,16 @@
+import org.json.simple.parser.JSONParser;
+
 /**
  * Created by morgane on 17/06/17.
  */
 public class ParseurJSON {
+
+    public ParseurJSON() {
+    }
+
+
+    public void getDonnee(){
+        JSONParser parser = new JSONParser();
+
+    }
 }
