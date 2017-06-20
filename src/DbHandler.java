674754d5597d999f5e08bd22c8722fabb8fc12fb
morgane <mcabrol21@gmail.com>
2017-06-20 12:36:01 +0200

    test name

--- src/DbHandler.java
+++ src/DbHandler.java
@@ -1,5 +1,10 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.lightcouch.CouchDbClient;
 import org.lightcouch.CouchDbClientBase;
 import org.lightcouch.CouchDbInfo;
@@ -11,17 +16,27 @@ public class DbHandler {
 	
 	public void testing() {
 		 
-		CouchDbClientBase db = new CouchDbClient("if25_tweets", true, "http", "localhost", 5984, "anta", "password");
+		CouchDbClientBase db = new CouchDbClient("if25_tweets", true, "http", "localhost", 5984, "root", "root");
 		//CouchDbClient dbClient = new CouchDbClient("if25_tweets", true, "http", "localhost", 5984, "anta", "password");
-		
-		System.out.print(db.view("_all_docs")
+
+	/*	System.out.print(db.view("_all_docs")
 				.reduce(false)
 				.limit(10)
 				.includeDocs(true)
-				.query(Object.class));
+				.query(Object.class));*/
 		
-		CouchDbInfo info = new CouchDbInfo();
-		System.out.println(info.getDbName());
+		//CouchDbInfo info = new CouchDbInfo();
+		//System.out.println(info.getDbName());
+
+		List<JsonObject> jsonList = db.view("userview/users-view").query(JsonObject.class);
+		Set<JsonObject> hs = new HashSet<>();
+		hs.addAll(jsonList);
+		jsonList.clear();
+		jsonList.addAll(hs);
+		for (int i=0; i< 50; i++){
+			JsonObject jsonObject = jsonList.get(i).get("key").getAsJsonObject();
+			System.out.println(jsonObject.get("screen_name"));
+		}
 	}
 
 }
